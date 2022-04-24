6fce0570c9277ed848f13451694c8b0efd7fef4e
Szyler <Szyler@Szyler.com>
2022-04-24 12:52:43 +0200

    Fixed github links to Ascension

--- src/main/scala/wowchat/WoWChat.scala
+++ src/main/scala/wowchat/WoWChat.scala
@@ -13,7 +13,7 @@ import scala.io.Source
 
 object WoWChat extends StrictLogging {
 
-  private val RELEASE = "v1.3.8"
+  private val RELEASE = "v2.0.0"
 
   def main(args: Array[String]): Unit = {
     logger.info(s"Running WoWChat - $RELEASE")
@@ -78,7 +78,7 @@ object WoWChat extends StrictLogging {
 
   private def checkForNewVersion = {
     // This is JSON, but I really just didn't want to import a full blown JSON library for one string.
-    val data = Source.fromURL("https://api.github.com/repos/fjaros/wowchat/releases/latest").mkString
+    val data = Source.fromURL("https://github.com/Szyler/AscensionChat/releases/latest").mkString
     val regex = "\"tag_name\":\"(.+?)\",".r
     val repoTagName = regex
       .findFirstMatchIn(data)
@@ -89,7 +89,7 @@ object WoWChat extends StrictLogging {
       logger.error( "~~~ !!!                YOUR WoWChat VERSION IS OUT OF DATE                !!! ~~~")
       logger.error(s"~~~ !!!                     Current Version:  $RELEASE                      !!! ~~~")
       logger.error(s"~~~ !!!                     Repo    Version:  $repoTagName                      !!! ~~~")
-      logger.error( "~~~ !!! RUN git pull OR GO TO https://github.com/fjaros/wowchat TO UPDATE !!! ~~~")
+      logger.error( "~~~ !!!         GO TO https://github.com/Szyler/AscensionChat TO UPDATE      !!! ~~~")
       logger.error( "~~~ !!!                YOUR WoWChat VERSION IS OUT OF DATE                !!! ~~~")
     }
   }
